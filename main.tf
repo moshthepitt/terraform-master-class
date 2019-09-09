55a948bddf74fa5fb53fe11265454228ce1fed32
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-09-09 19:54:09 +0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "example-MOSH" {
+  ami           = "ami-05c1fa8df71875112"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "moshthepitt"
+      Purpose = "Dickson's master class example"
+  }
+}
